ffc36e8057df4a2e1ad76771cf1e238ab13c1be5
ramilmavlihanov <Ramil040201@gmail.com>
2024-07-28 17:46:44 +0300

    Initial commit

---
+++ homework2.py
@@ -0,0 +1,6 @@
+countDoneHomeworks = 12
+countSpentHours = 1.5
+courseName = 'Python'
+timePerTask = countSpentHours/countDoneHomeworks
+
+print(f'Course: {courseName},\nTotal tasks: {countDoneHomeworks},\nSpent hours: {countSpentHours},\nAverage execution time: {timePerTask}')
